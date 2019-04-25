e2c17d42245c4694be7a0d3093690ae33daa0c6b
xubiwei <913161504@qq.com>
2019-04-25 20:26:46 +0800

    test

---
+++ XBWTool/Tool.h
@@ -0,0 +1,17 @@
+//
+//  Tool.h
+//  XBWTool
+//
+//  Created by 冯思 on 2019/4/25.
+//  Copyright © 2019 冯思. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Tool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XBWTool/Tool.m
@@ -0,0 +1,13 @@
+//
+//  Tool.m
+//  XBWTool
+//
+//  Created by 冯思 on 2019/4/25.
+//  Copyright © 2019 冯思. All rights reserved.
+//
+
+#import "Tool.h"
+
+@implementation Tool
+
+@end
